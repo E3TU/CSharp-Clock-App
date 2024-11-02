ee3fbe0b8e7d91f19a24383a8b2ae1260207ebc4
Eetu <99178022+E3TU@users.noreply.github.com>
2024-11-02 21:38:20 +0200

    Changed folder structure

---
+++ ClockApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ClockApp
+{
+    class ClockApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ClockApp";
+            
+            Console.Clear();
+            Console.CursorVisible = false;
+            
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                
+                string time24h = now.ToString("HH:mm:ss");
+                
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(time24h);
+                Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
